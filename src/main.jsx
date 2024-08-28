e1d8fbf2701eeeb961a1588c1d8069d2c8339afe
alimuhammed_Dev <muhammedaliali2020@gmail.com>
2024-08-28 04:02:29 +0100

    final commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,12 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
-import { StoreContext } from './Context/StoreContext.jsx';
+import StoreContextProvider from './Context/StoreContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <StoreContext>
+    <StoreContextProvider>
       <App />
-    </StoreContext>
+    </StoreContextProvider>
   </BrowserRouter>
 )
